6189cf85e5dd4db1509463f5a38b5a754bc8d553
Adrian Scripca <benishor@gmail.com>
2020-06-18 18:31:25 +0300

    Added FPU initialization for cube startup

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,16 +1,34 @@
 #include "DigitalIn.h"
 #include "DigitalOut.h"
 
-//#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-//  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-//#endif
-
 void delay() {
 	for (uint32_t i = 0; i < 500000; i++) {
 	}
 }
 
+// need to enable FPU access, otherwise FP operations will fail
+#define CPACR (uint32_t *)0xE000ED88
+
+//__attribute__((constructor)) void initializeFpu(void) {
+//	*CPACR |= ((3UL << 20) | (3UL << 22)); // set CP10 and CP11 to full access
+//}
+
+[[gnu::constructor(0)]] void initializeFpu(void) {
+	*CPACR |= ((3UL << 20) | (3UL << 22)); // set CP10 and CP11 to full access
+}
+
+//extern "C" {
+//
+//	void SystemInit() {
+//		*CPACR |= ((3UL << 20) | (3UL << 22)); // set CP10 and CP11 to full access
+//	}
+//}
+
 int main(void) {
+
+	float a = 1.2345f;
+	float b = 2.3456f;
+	float c = a / b;
 
 	DigitalIn button(Port::I, PinNumber::Pin11, PinPullUpMode::None);
 	DigitalOut led(Port::I, PinNumber::Pin1, PinOutputType::PushPull);
@@ -19,6 +37,7 @@ int main(void) {
 		if (button.read()) {
 			delay();
 			led.toggle();
+			c += 1.1f;
 		}
 	}
 }
